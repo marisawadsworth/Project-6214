e52aa62313a131ac16044ab8c825aaf5affaa94b
Jeffrey Kranenburg <jeff_mba@Jeffreys-MacBook-Air.local>
2019-06-08 10:33:51 +1200

    Vue -> vue

---
+++ docs/.vuepress/theme/layouts/About/AboutLeft.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Content />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AboutLeft'
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
